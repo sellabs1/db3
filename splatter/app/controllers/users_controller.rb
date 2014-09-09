39324951195281849f7d8a4996d7c5fd163ed882
Bryce <t.h.i.r.d_3y3@hotmail.com>
2014-09-09 22:55:52 +0000

    Completed lab 8.2

--- splatter/app/controllers/users_controller.rb
+++ splatter/app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params(params[:user]))
+    @user = User.new(user_params(params))
 
     if @user.save
       render json: @user, status: :created, location: @user
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    if @user.update(user_params(params[:user]))
+    if @user.update(user_params(params))
       head :no_content
     else
       render json: @user.errors, status: :unprocessable_entity
